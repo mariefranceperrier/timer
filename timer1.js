47f73bfbf2fccfead1e24f9e6032febefbaf5123
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-06 18:35:45 -0500

    added file timer1.js

---
+++ timer1.js
@@ -0,0 +1,25 @@
+const delay = process.argv.slice(2).sort(); 
+
+delay.forEach((time) => {
+  const parsedTime = parseInt(time);
+
+  if (delay.length > 0 && parsedTime >= 0 && !isNaN(parsedTime)) {
+    
+    setTimeout(() => {
+      process.stdout.write('.');
+    }, parseInt(time) * 1000);
+  }
+});
+
+setTimeout(() => {
+  console.log();
+}, parseInt(delay[delay.length -1]) * 2000);
+
+
+
+
+
+
+
+
+
